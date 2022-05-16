9ee4ec1f94c8554af4d3a91256b871dcb05f2218
Saad Adel <saad.adel539@yahoo.com>
2022-05-17 01:28:21 +0200

    reduce TITLE regex to have from 1 to 25 chars in each word

--- backend/utils/regex.py
+++ backend/utils/regex.py
@@ -1,5 +1,5 @@
 URL=r'^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$' # URL format
-TITLE=r'^(\w*\s?\w*){1,3}$' # String consists of only letters, digits, and spaces
+TITLE=r'^(\w{0,25}\s?\w{0,25}){1,3}$' # String consists of only letters, digits, and spaces
 DESC=r'^(\w*\s?\w*){1,25}$' # Long string consists of only letters, digits, and spaces
 STRING=r'^\w{1,15}$' # Only digits and letters between 1 and 50
 DIGITS=r'^\d{1,50}$' # Only digits between 1 and 50
