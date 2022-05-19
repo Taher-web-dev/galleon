5e1279e0c524d5df02c070bb3b67f88090fd114e
Ahmed Shahwan <ahmed.shahwan@startappz.com>
2022-05-20 00:44:27 +0200

    User/create much better docs

--- backend/api/user/response_models.py
+++ backend/api/user/response_models.py
@@ -37,6 +37,24 @@ class InvalidCredentialsErrorResponse(ApiResponse):
 class UserProfile(BaseModel):
     id: int
     name: str
-    msisdn: str = Field(..., regex=rgx.MSISDN)
-    email: Optional[str] = Field(None, regex=rgx.EMAIL)
-    profile_pic_url: Optional[str] = Field(None, regex=rgx.URL)
+    msisdn: str = Field(..., regex=rgx.MSISDN, max_length=20)
+    email: str | None = Field(None, regex=rgx.EMAIL, max_length=40)
+    profile_pic_url: str | None = Field(None, regex=rgx.URL)
+
+
+class UserProfileResponse(ApiResponse):
+    data: UserProfile
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "status": "success",
+                "data": {
+                    "id": 1,
+                    "name": "Ahmed Shahwan",
+                    "msisdn": "12345678933",
+                    "email": "ahmed.shahwan@startappz.com",
+                    "profile_pic_url": "https://example.com/fake_pic.jpg",
+                },
+            }
+        }
