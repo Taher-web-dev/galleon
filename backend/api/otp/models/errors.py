00c3c3ad8d5c02f77be3aba44f1aad0f7ae5570e
Ahmed Shahwan <ahmed.shahwan@startappz.com>
2022-05-25 10:55:33 +0200

    Move models to otp/models

--- backend/api/otp/models/errors.py
+++ backend/api/otp/models/errors.py
@@ -12,25 +12,25 @@ SMS_ERROR = Error(
     message="There are no SMS GateWay to fulfill your request.",
 )
 
-INVALID_OTP_REQUEST_ID = Error(
+INVALID_REQUEST_ID = Error(
     type="otp",
     code=304,
     message="Something is the wrong! the OTP request you submitted is invalid.",
 )
 
-INVALID_OTP_REQUEST_FORMAT = Error(
+INVALID_REQUEST_FORMAT = Error(
     type="otp",
     code=305,
     message="The submitted OTP format you shared is invalid format.",
 )
 
-INVALID_OTP_CONFIRMATION = Error(
+INVALID_CONFIRMATION = Error(
     type="otp",
     code=306,
     message="Something is wrong! the OTP request you confirmation is invalid.",
 )
 
-OTP_MISMATCH = Error(
+INVALID_OTP = Error(
     type="otp",
     code=307,
     message="Something is wrong! the OTP request you confirmation does not match the request.",
