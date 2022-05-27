e3115b4888216d91cb9e425d5588fd36d0a3bbed
stephen_rawson <stephen.rawson@oliverwyman.com>
2022-05-27 10:19:45 +0400

    Postpaid SIM status, dashboard message

--- backend/api/number/cms.py
+++ backend/api/number/cms.py
@@ -3,6 +3,7 @@ NORMAL = "NORMAL"
 
 # NOT NORMAL BUT CAN ACCESS WITH WARNING
 WARN_RECHARGE = "WARN_RECHARGE"
+WARN_MUST_PAY_BILL = "WARN_MUST_PAY_BILL"
 WARN_MUST_RECONNECT = "WARN_MUST_RECONNECT"
 
 # CANNOT ACCESS - GENERIC BUT KNOWN
@@ -47,6 +48,17 @@ SIM_STATUS_LOOKUP_PREPAID_CONSUMER_MOBILE = {
     "B06": {0: BLOCK_MUST_ACTIVATE},
 }
 
+SIM_STATUS_LOOKUP_POSTPAID_CONSUMER_MOBILE = {
+    "B01": {"unhandled": NORMAL},
+    "B02": {"unhandled": BLOCK_DISCONNECTED},
+    "B03": {
+        "dunning_suspend": WARN_MUST_PAY_BILL,
+        "suspend_due_to_dunning": WARN_MUST_PAY_BILL,
+        "unhandled": BLOCK_DISCONNECTED,
+    },
+    "B04": {"unhandled": BLOCK_DISCONNECTED},
+}
+
 # Other warnings not tied to SIM status itself
 WARN_NOT_4G_COMPATIBLE = "WARN_NOT_4G_COMPATIBLE"
 
@@ -69,8 +81,16 @@ SIM_NBA_LOOKUP = {
         "href_text_ar": "",
         "href_text_kd": "",
     },
+    WARN_MUST_PAY_BILL: {
+        "href": "",
+        "message_en": "",
+        "message_ar": "",
+        "message_kd": "",
+        "href_text_en": "",
+        "href_text_ar": "",
+        "href_text_kd": "",
+    },
 }
-
 
 ELIGIBLE_PRIMARY_OFFERINGS: list[int] = [
     100100,
@@ -107,4 +127,26 @@ NEGCRED_LOOKUP: dict[int, int] = {
     2086109: 8000,
     2086110: 10000,
     2086111: 12000,
+}
+
+POSTPAID_PRIME_PRIMARY_OFFERINGS: int = []
+
+ZAINFI_NBA: dict = {
+    "href": "https://apps.iq.zain.com/zain-fi",
+    "message_en": "Did you hear about our new Zain-Fi app?",
+    "message_ar": "Did you hear about our new Zain-Fi app?",
+    "message_kd": "Did you hear about our new Zain-Fi app?",
+    "href_text_en": "View app",
+    "href_text_ar": "View app",
+    "href_text_kd": "View app",
+}
+
+POSTPAID_PRIME_NBA: dict = {
+    "href": "https://www.iq.zain.com/ar/new-prime-line-strongest-postpaid-line-zain ",
+    "message_en": "Upgrade to Postpaid Prime now!",
+    "message_ar": "Upgrade to Postpaid Prime now!",
+    "message_kd": "Upgrade to Postpaid Prime now!",
+    "href_text_en": "Check it out",
+    "href_text_ar": "Check it out",
+    "href_text_kd": "Check it out",
 }
