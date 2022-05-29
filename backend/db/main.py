291740343695d6da14a7359243dbedcf6c9a8f8c
Kefah Issa <kefah@imx.sh>
2022-05-29 17:13:09 +0300

    Added keep-alive to postgres connection

--- backend/db/main.py
+++ backend/db/main.py
@@ -4,5 +4,14 @@ from utils.settings import settings
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-engine = create_engine(settings.database_url)
+engine = create_engine(
+    settings.database_url,
+    pool_pre_ping=True,
+    connect_args={
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5,
+    },
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
