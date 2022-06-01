affaa5d0ed1292c01d3a8656f21ca43c76accccf
Kefah Issa <kefah@imx.sh>
2022-06-01 14:38:09 +0300

    wip for rsync

--- rsync.sh
+++ rsync.sh
@@ -5,6 +5,9 @@ for server in galleon_app_uat_1 galleon_app_uat_2; do
   ssh $server podman stop galleon-middleware
   rsync -av --exclude 'secrets.env' --exclude "logs" --exclude "__pycache__" --exclude ".pytest_cache" backend ${server}:/home/auser/containers/middleware/
   #ssh $server sed -i 's/@127.0.0.1\\/galleon/@10.90.51.194\\/galleon/g' /home/auser/containers/middleware/backend/secrets.env
+  #ssh $server sed -i 's/^ZEND_API=.*$/ZEND_API=$(ifconfig ens192 | grep inet  | awk "{print \$2}")\//g' /home/auser/containers/middleware/backend/secrets.env
+  #ssh $server sed -i 's/^MOCK_ZAIN_API=.*$/MOCK_ZAIN_API=false/g' /home/auser/containers/middleware/backend/secrets.env
+  #ssh $server sed -i 's/@127.0.0.1\\/galleon/@10.90.51.194\\/galleon/g' /home/auser/containers/middleware/backend/secrets.env
   #ssh $server sed -i 's/^MOCK_ZAIN_API=.*$/MOCK_ZAIN_API=false/g' /home/auser/containers/middleware/backend/secrets.env
   ssh $server podman start galleon-middleware
 
