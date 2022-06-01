55368bcc084dfaac570fc95afc86f16b2c511a90
Kefah Issa <kefah@imx.sh>
2022-06-01 13:22:19 +0300

    Do not update secrets

--- rsync.sh
+++ rsync.sh
@@ -3,9 +3,9 @@
 for server in galleon_app_uat_1 galleon_app_uat_2; do 
   echo $server
   ssh $server podman stop galleon-middleware
-  rsync -av --exclude "logs" --exclude "__pycache__" --exclude ".pytest_cache" backend ${server}:/home/auser/containers/middleware/
-  ssh $server sed -i 's/@127.0.0.1\\/galleon/@10.90.51.194\\/galleon/g' /home/auser/containers/middleware/backend/secrets.env
-  ssh $server sed -i 's/^MOCK_ZAIN_API=.*$/MOCK_ZAIN_API=false/g' /home/auser/containers/middleware/backend/secrets.env
+  rsync -av --exclude 'secrets.env' --exclude "logs" --exclude "__pycache__" --exclude ".pytest_cache" backend ${server}:/home/auser/containers/middleware/
+  #ssh $server sed -i 's/@127.0.0.1\\/galleon/@10.90.51.194\\/galleon/g' /home/auser/containers/middleware/backend/secrets.env
+  #ssh $server sed -i 's/^MOCK_ZAIN_API=.*$/MOCK_ZAIN_API=false/g' /home/auser/containers/middleware/backend/secrets.env
   ssh $server podman start galleon-middleware
 
 done
