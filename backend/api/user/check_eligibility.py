c8cf20d63a84911603a6f0df679c0e407bb6980e
Kefah Issa <kefah@imx.sh>
2022-06-02 20:53:06 +0300

    Fixed check eligibility for mocked api

--- backend/api/user/check_eligibility.py
+++ backend/api/user/check_eligibility.py
@@ -1,8 +1,11 @@
 from api.number.zend import zend_sim
 from api.number.sim import get_unified_sim_status
+from utils.settings import settings
 
 
 def check_eligibility(msisdn: str):
     backend_sim_status = zend_sim(msisdn)
     unified_sim_status = get_unified_sim_status(backend_sim_status)
+    if settings.mock_zain_api:
+        return True
     return "BLOCK" not in unified_sim_status
