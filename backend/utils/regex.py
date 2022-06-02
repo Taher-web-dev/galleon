5548c7d6455cc4e484128814204343bfa5814b9d
Kefah Issa <kefah@imx.sh>
2022-06-02 20:53:32 +0300

    Fixed msisdn regix

--- backend/utils/regex.py
+++ backend/utils/regex.py
@@ -5,5 +5,5 @@ DESC = (
 )
 STRING = r"^\w{1,50}$"  # Only digits and letters between 1 and 50
 DIGITS = r"^\d{1,50}$"  # Only digits between 1 and 50
-MSISDN = r"^7\d{9}$"  # Only digits between 8 and 15
+MSISDN = r"^[1-9]\d{9}$"  # Exactly 10 digits, not starting with zero
 PASSWORD = r"^[\w.@#$%^&+=\-\s]{8,}$"  # Password format, at least 8 chars
