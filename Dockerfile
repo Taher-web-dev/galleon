04c04aa86f1840685ba105a3864b8370f6960ae9
Kefah Issa <kefah@imx.sh>
2022-06-02 20:52:24 +0300

    Switching to gunicorn

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ VOLUME ["/home"]
 ADD backend /home/backend/
 
 # Install required run-time packages
-RUN apk add --no-cach python3 py3-requests py3-jwt py3-six py3-sqlalchemy libpq py3-jinja2 py3-pydantic py3-bcrypt py3-psycopg2 py3-wheel py3-passlib
+RUN apk add --no-cach python3 py3-requests py3-jwt py3-six py3-sqlalchemy libpq py3-jinja2 py3-pydantic py3-bcrypt py3-psycopg2 py3-wheel py3-passlib py3-gunicorn
 
 # Install required pips (from backend/requirments.txt) along with disposable build tools
 RUN apk add --no-cache --virtual devstuff py3-pip  \
--- backend/run.sh
+++ backend/run.sh
@@ -5,4 +5,7 @@ export LOG_PATH=${BASE}/logs
 LISTENING_PORT=$(grep -i '^LISTENING_PORT' $BACKEND_ENV | sed 's/^[^=]* *= *//g' | tr -d '"')
 LISTENING_HOST=$(grep -i '^LISTENING_HOST' $BACKEND_ENV | sed 's/^[^=]* *= *//g' | tr -d '"')
 mkdir -p $LOG_PATH
-uvicorn --env-file $BACKEND_ENV --port $LISTENING_PORT --app-dir ${BASE} --host $LISTENING_HOST --reload main:app
+# uvicorn --env-file $BACKEND_ENV --port $LISTENING_PORT --app-dir ${BASE} --host $LISTENING_HOST --reload main:app
+# gunicorn main:app -w $(nproc --all) -k uvicorn.workers.UvicornWorker -b $LISTENING_HOST':'$LISTENING_PORT
+cd $(dirname "$(realpath $0)")
+gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker -b $LISTENING_HOST':'$LISTENING_PORT
