60d593ee5f3b121c225dc535a70ad86ba0cf642d
Kefah Issa <kefah@imx.sh>
2022-06-06 09:04:16 +0300

    Start using the concurrent log handler

--- backend/utils/logger.py
+++ backend/utils/logger.py
@@ -1,10 +1,16 @@
 import logging
 import logging.handlers
+
 from utils.settings import settings
+from concurrent_log_handler import ConcurrentRotatingFileHandler
+
 
 logger = logging.getLogger(settings.app_name)
 logger.setLevel(logging.INFO)
-log_handler = logging.handlers.RotatingFileHandler(
-    filename=f"{settings.log_path}/x-ljson.log", maxBytes=5_000_000, backupCount=10
+log_handler = ConcurrentRotatingFileHandler(
+    f"{settings.log_path}/x-ljson.log", "a", 5_000_000, 10
 )
+#log_handler = logging.handlers.RotatingFileHandler(
+#    filename=f"{settings.log_path}/x-ljson.log", maxBytes=5_000_000, backupCount=10
+#)
 logger.addHandler(log_handler)
