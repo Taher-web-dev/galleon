de07d06e0250dca7daa2aa121d7eabac55023c5b
Kefah Issa <kefah@imx.sh>
2022-06-08 06:53:23 +0300

    make the output of curl.sh pretty and brief

--- backend/curl.sh
+++ backend/curl.sh
@@ -8,31 +8,31 @@ API_URL=http://127.0.0.1:8080/api
 
 CT="content-type: application/json"
 
-echo "Delete old data"
+echo -n -e "Delete old data : \t"
 psql -h 127.0.0.1 -U galleon galleon -c 'delete from users; delete from otp;'
 
-echo "Request OTP"
-time curl -s -H "$CT" -d "{\"msisdn\":\"${MSISDN}\"}" ${API_URL}/otp/request | jq
+echo -n -e "Request OTP : \t\t"
+curl -s -H "$CT" -d "{\"msisdn\":\"${MSISDN}\"}" ${API_URL}/otp/request | jq .status
 OTP=$(psql -q --csv -t -h 127.0.0.1 -U galleon galleon -c "select code from otp where msisdn='${MSISDN}'; ")
 
-echo "OTP is $OTP"
+#echo "OTP is $OTP"
 CONF=$(curl -s -H "$CT" -d "{\"msisdn\":\"${MSISDN}\", \"code\": $OTP}" ${API_URL}/otp/confirm | jq .data.confirmation | tr -d '"')
-echo "Confirmation $CONF"
+#echo "Confirmation $CONF"
 
-echo "Creating user"
-curl -s -H "$CT" -d "{\"name\":\"${NAME}\", \"msisdn\":\"${MSISDN}\", \"password\":\"${PASSWORD}\", \"otp_confirmation\":\"${CONF}\"}" $API_URL/user/create | jq
+echo -n -e "Creating user : \t"
+curl -s -H "$CT" -d "{\"name\":\"${NAME}\", \"msisdn\":\"${MSISDN}\", \"password\":\"${PASSWORD}\", \"otp_confirmation\":\"${CONF}\"}" $API_URL/user/create | jq .status
 
-echo "Login"
-curl -s -H "$CT" -d "{\"msisdn\":\"${MSISDN}\", \"password\":\"${PASSWORD}\"}" $API_URL/user/login | jq
+echo -n -e "Login : \t\t"
+curl -s -H "$CT" -d "{\"msisdn\":\"${MSISDN}\", \"password\":\"${PASSWORD}\"}" $API_URL/user/login | jq .status
 TOKEN=$(curl -s -H "$CT" -d "{\"msisdn\":\"${MSISDN}\", \"password\":\"${PASSWORD}\"}" $API_URL/user/login | jq -r .data.access_token)
-echo "JWT Token $TOKEN"
+#echo "JWT Token $TOKEN"
 
-echo "Update profile"
-curl -s -H "$CT" -H "Authorization: Bearer $TOKEN" -X PATCH -d "{\"email\":\"${EMAIL}\"}" $API_URL/user/profile | jq
+echo -n -e "Update profile : \t"
+curl -s -H "$CT" -H "Authorization: Bearer $TOKEN" -X PATCH -d "{\"email\":\"${EMAIL}\"}" $API_URL/user/profile | jq .status
 
-echo "Get profile"
-curl -s -H "$CT" -H "Authorization: Bearer $TOKEN" $API_URL/user/profile | jq
+echo -n -e "Get profile : \t\t"
+curl -s -H "$CT" -H "Authorization: Bearer $TOKEN" $API_URL/user/profile | jq .status
 
-echo "Delete"
-curl -s -H "$CT" -X DELETE -H "Authorization: Bearer $TOKEN" -d '{}' $API_URL/user/delete | jq
+echo -n -e "Delete : \t\t"
+curl -s -H "$CT" -X DELETE -H "Authorization: Bearer $TOKEN" -d '{}' $API_URL/user/delete | jq .status
 
